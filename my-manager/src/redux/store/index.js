e55c787645aa3a4c8097937c577d6671459a914a
anluofly <anluofly@163.com>
2019-04-18 15:01:38 +0800

    修改文件目录结构

--- my-manager/src/redux/store/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * 引入createStore创建Store
- */
-
- import { createStore } from 'redux'
- import reducer from './../reducer'
- import { composeWidthDevTools } from 'redux-devtools-extension'
-
- export default () => createStore(reducer, composeWidthDevTools())
